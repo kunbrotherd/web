e8596bb0740e3e6c818a1895de8172722acbca06
kunbrotherd <13772093835@163.com>
2024-06-07 11:38:00 +0800

    Update conftest.py

--- webSelenium/conftest.py
+++ webSelenium/conftest.py
@@ -7,7 +7,7 @@ from selenium.webdriver.chrome.service import Service
 @allure.feature('打开浏览器')
 @pytest.fixture(scope='session', autouse=True)
 def init_driver(request):
-    driver_path = r'C:\Users\86137\AppData\Local\Google\Chrome\Application\chromedriver.exe'
+    driver_path = os.path.join(get_par_path(), 'driver/chromedriver.exe')
     # 创建一个Service对象，传入chromedriver的路径
     service = Service(driver_path)
     # 创建一个webriver对象，传入service对象
